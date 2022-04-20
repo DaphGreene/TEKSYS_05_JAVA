d08fcca27a1773db9df291049e2b3e6265e6c776
DaphneGlitches <daphnergreene@gmail.com>
2022-04-20 11:44:38 -0700

    Pushing BankingProject up

---
+++ BankingProject/src/Main/Main.java
@@ -0,0 +1,19 @@
+package Main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello from WEDNESDAY, 04/20/2022" + " project.");
+        
+        Employee emp1 = new Employee("John", "Smith", 12345, "Sales");
+        Employee emp2 = new Employee("Jane", "Smith", 54321, "Sales");
+        Manager manager1 = new Manager("Candice", "Smith", 4533, "Manager", "Sales TimeCard");
+        
+        System.out.println(emp1);
+        System.out.println(emp2);
+        System.out.println(manager1);
+        
+    }
+
+}
